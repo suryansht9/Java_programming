a967632a636f04a1e419c394bebfe3c89bc0842e
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-10-02 20:19:29 +0530

    Add files via upload

---
+++ tablemultiplication.java
@@ -0,0 +1,9 @@
+//write table of 7 using the for loop
+public class tablemultiplication {
+    public static void main(String[] args) {
+        for(int i=7;i<=70;i+=7){
+            System.out.println(i);
+        }
+    }
+    
+}
