f71fa67336f3dd4b2bd72125d220260abcef24a6
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-10-02 20:19:11 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,23 @@
+//WAP OF IF ELSE IF LADDER
+/*public class ifelse{
+    public static void main(String[] args) {
+        int num=99;
+        if (num%2==0){System.out.println("90 is divisble by 2");}
+        else if(num%3==0){System.out.println("90 is not divisble y 3");}
+        else{System.out.println("please enter valid number");}
+    }
+}*/
+
+//Find the largest of three numbers.
+
+/*
+public class ifelse{
+    public static void main(String[] args) {
+    int num=456;
+    if (num>998 && num<1000){System.out.println("your given value is the largest number of 3 digit");}
+    else{System.out.println("your given value is not the largest number of 3 digit");}    
+    }
+}
+*/
+
+
