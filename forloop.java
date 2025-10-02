63c4380199a967a62b2b982f64a002ef818f1518
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-10-02 20:19:50 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,18 @@
+//WAP OF FOR LOOP print 1 to 10
+
+/*public class forloop {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
+*/
+
+//Print even numbers from 1 to 100
+public class forloop {
+    public static void main(String[] args) {
+        for(int i=2;i<=100;i+=2){
+            System.out.println(i);}
+        }}
