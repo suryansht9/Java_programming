f9b1b4822142b1b17fd868b62b49a68909e0836b
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-10-10 19:32:43 +0530

    Add files via upload

---
+++ bitwiseoperator.java
@@ -0,0 +1,11 @@
+public class bitwiseoperator {
+    
+    public static void main(String[] args) {
+        int a=5; //0101
+        int b=6; //0110
+        System.out.println("a & b is"+(a&b));
+        System.out.println("a | b is"+(a|b));
+        System.out.println(" a ^ b is "+(a^b));
+        
+    }
+}
