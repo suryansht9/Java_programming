5eb845e8a6569e4d132cb09ff21a6afc0241720c
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-10-10 19:32:24 +0530

    Add files via upload

---
+++ booleanoperator.java
@@ -0,0 +1,10 @@
+public class booleanoperator {
+    public static void main(String[] args) {
+        boolean a=true;
+        boolean b=false;
+        System.out.println("a && b is"+(a&&b));
+        System.out.println("a || b is"+(a||b));
+        System.out.println(" a not is "+(!a));
+    }
+    
+}
